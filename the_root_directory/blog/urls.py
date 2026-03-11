d06ce279322bc9de7b14dfb360119f95f087aec3
garzuzo <garzuxo@gmail.com>
2026-03-10 23:28:34 -0300

    feat(owner): add owner registry
    Resolves #10

--- the_root_directory/blog/urls.py
+++ the_root_directory/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from blog.views import FileDetailView, FilesView
+from blog.views import FileDetailView, FilesView, LoginView, RegisterView
 
 from . import views
 
@@ -8,4 +8,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("files/", FilesView.as_view(), name="files"),
     path("files/<int:pk>/<slug:slug>/", FileDetailView.as_view(), name="file_detail"),
+    path("owner/login/", LoginView.as_view(), name="login"),
+    path("owner/register/", RegisterView.as_view(), name="register"),
+    path("owner/welcome/", views.welcome, name="welcome"),
 ]
